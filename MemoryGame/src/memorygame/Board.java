d528ac2325c5cc4e950d85a5d2c59776249f66e6
Skylar Neilson <skylarjn@gmail.com>
2014-01-25 19:25:48 -0700

    Update Board.java

--- MemoryGame/src/memorygame/Board.java
+++ MemoryGame/src/memorygame/Board.java
@@ -23,3 +23,67 @@ public class Board {
     String j;
     
 }
+public static void main(String[] args) {
+      MemoryGame myGame = new MemoryGame();
+      myGame.getCardA();
+      myGame.getCardB();
+      myGame.getCardC();
+      myGame.getCardD();
+      myGame.getCardE();
+      myGame.getCardF();
+      myGame.getCardG();
+      myGame.getCardH();
+      myGame.getCardI();
+      myGame.getCardJ();
+        // TODO code application logic here
+    }
+    
+    public void getCardA(){
+    Scanner input = new Scanner(System.in);
+    System.out.println(this.a);
+     }
+    
+    public void getCardB(){
+    Scanner input = new Scanner(System.in);
+    System.out.println(this.b);
+     }
+     
+     public void getCardC(){
+    Scanner input = new Scanner(System.in);
+    System.out.println(this.c);
+     }
+     
+     public void getCardD(){
+    Scanner input = new Scanner(System.in);
+    System.out.println(this.d);
+     }
+     
+     public void getCardE(){
+    Scanner input = new Scanner(System.in);
+    System.out.println(this.e);
+     }
+     
+     public void getCardF(){
+    Scanner input = new Scanner(System.in);
+    System.out.println(this.f);
+     }
+     
+     public void getCardG(){
+    Scanner input = new Scanner(System.in);
+    System.out.println(this.g);
+     }
+     
+     public void getCardH(){
+    Scanner input = new Scanner(System.in);
+    System.out.println(this.h);
+     }
+     
+     public void getCardI(){
+    Scanner input = new Scanner(System.in);
+    System.out.println(this.i);
+     }
+     
+     public void getCardJ(){
+    Scanner input = new Scanner(System.in);
+    System.out.println(this.j);
+     }
