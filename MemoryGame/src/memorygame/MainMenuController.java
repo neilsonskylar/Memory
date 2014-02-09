52dc4f5ba3ec5b64c6b76b0932942c01dbb04ecc
Skylar Neilson <Skylar@Megamind>
2014-02-08 17:55:23 -0700

    Merge origin/master
    Conflicts:
    	MemoryGame/src/memorygame/Board.java
    	MemoryGame/src/memorygame/Player.java

---
+++ MemoryGame/src/memorygame/MainMenuController.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package memorygame;
+
+/**
+ *
+ * @author Skylar
+ */
+public class MainMenuController {
+    
+public MainMenuController() {
+        
+    } 
+
+    public void displayStartGame() {
+        System.out.println();
+        this.displayHelpBorder();             
+        System.out.println( 
+                "\t Start Game.");
+        displayHelpBorder();
+    }
+    
+    
+        
+    public void displayHelpMenu() {
+        System.out.println();
+        displayHelpBorder();     
+        System.out.println( 
+                 "HelpMenuControl. "
+                 ); 
+        displayHelpBorder();
+    }
+            
+    public void displayPlayer() {
+        System.out.println();
+        displayHelpBorder();     
+        System.out.println( 
+                "\t Player Input "
+                 ); 
+        displayHelpBorder();
+    }
+    
+        
+
+                     
+    
+    public void displayHelpBorder() {       
+        System.out.println(
+        "\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }    
+}
---
+++ MemoryGame/src/memorygame/MainMenuView.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package memorygame;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Skylar
+ */
+public class MainMenuView {
+private final static String[][] menuItems = {
+        {"A", "Start Game"},
+        {"B", "Help Menu"},
+        {"C", "Player"},
+        {"Q", "Quit Help"}        
+    };
+    
+    // Create instance of the HelpMenuControl (action) class
+    private MainMenuController MainMenuController = new MainMenuController();
+    
+    // default constructor
+    public MainMenuView() {
+        
+    } 
+    
+    // display the help menu and get the end users input selection
+    public void getInput() {       
+              
+        String command;
+        Scanner inFile = new Scanner(System.in);
+        
+        do {
+            
+            this.display(); // display the menu
+            
+            // get commaned entered
+            command = inFile.nextLine();
+            command = command.trim().toUpperCase();
+            
+            switch (command) {
+                case "A":
+                    this.MainMenuController.displayStartGame();
+                    break;
+                case "B":
+                    this.MainMenuController.displayHelpMenu();
+                    break;                  
+                case "C":
+                    this.MainMenuController.displayPlayer();
+                    break;
+                case "Q": 
+                    break;
+                default: 
+                    new MemoryGameError().displayError("Invalid command. Please enter a valid command.");
+                    continue;
+            }
+        } while (!command.equals("Q"));  
+        
+         return;
+    }
+
+        // displays the help menu
+    public final void display() {
+        System.out.println("\n\t===============================================================");
+        System.out.println("\tEnter the letter associated with one of the following commands:");
+
+        for (int i = 0; i < MainMenuView.menuItems.length; i++) {
+            System.out.println("\t   " + menuItems[i][0] + "\t" + menuItems[i][1]);
+        }
+        System.out.println("\t===============================================================\n");
+    }
+  
+}
+ 
