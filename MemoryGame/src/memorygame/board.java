453b5ce19d59bd9f37f7329a33f213a86c0ef9eb
WoodwardAaron <aaronthebaron1@gmail.com>
2014-02-15 19:04:18 -0700

    Update board.java

--- MemoryGame/src/memorygame/board.java
+++ MemoryGame/src/memorygame/board.java
@@ -14,3 +14,27 @@ public class Grid {
    private Card[] cards;
    
 }
+int indx;
+        	for (int r = 0 ; r < SIZE ; r++)
+        	{
+              	for (int c = 0 ; c < SIZE ; c++)
+              	{
+                    	indx = r * SIZE + c;
+                    	card [indx] = new Card ();
+              	  	. . .
+              	}
+        	}
+
+         
+         for (int i = 1 ; i <= SIZE * SIZE / 2 ; i++)
+        	{
+              	for (int j = 0 ; j < 2 ; j++)
+              	{
+                    	do
+                    	{
+                          	indx = (int) (Math.random () * SIZE * SIZE);
+                    	}
+                    	while (card [indx].getValue () != 0);
+                    	card [indx].setValue (i);
+              	}
+        	}
