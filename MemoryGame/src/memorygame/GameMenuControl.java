f7b1efd9f202b76f830abc0dfc03f54b2f2dd3dc
Skylar Neilson <Skylar@10.18.148.139>
2014-02-20 17:21:12 -0700

    Merge origin/master
    Conflicts:
    	MemoryGame/src/memorygame/Board.java
    	MemoryGame/src/memorygame/Player.java

---
+++ MemoryGame/src/memorygame/GameMenuControl.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memorygame;
+
+/**
+ *
+ * @author aaron
+ */
+public class GameMenuControl {
+
+    private MemoryGame memorygame;
+    private Board board;
+
+    public GameMenuControl(Game game) {
+        this.memorygame = memorygame;
+        this.board = game.board;
+    }
+
+       
+    
+   public void takeTurn() {
+        System.out.println("\n\ttakeTurn() called");
+    }
+   
+       
+   public void displayBoard() {
+        System.out.println("\n\tdisplayBoard() called");
+    }
+   
+   
+    public void startNewGame() {
+        System.out.println("\n\tstartNewGame() called");
+    }
+    
+    
+            
+    public void displayPreferencesMenu() {
+        GamePreferencesMenuView gamePreferencesMenu = new GamePreferencesMenuView(this.game);
+        gamePreferencesMenu.getInput();
+    }
+    
+    
+    public void displayStatistics() {
+        String playerAStatistics = this.memorygame.playerA.getPlayerStastics();
+        System.out.println("\n\t++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("\t " + playerAStatistics);
+        
+    }
+    
+    public void displayHelpMenu() {
+        HelpMenuView helpMenu = new HelpMenuView();
+        helpMenu.getInput();
+    }
+    
+
+    
+    
+    
+}
